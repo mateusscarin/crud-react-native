4ecc5c35cab11c55a1517e07eb9025826a54e37f
mateusscarin <mateus.scarin@gmail.com>
2024-05-26 16:06:16 -0300

    feat: firebase config connection

---
+++ service/connectionFirebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+  };
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
+
